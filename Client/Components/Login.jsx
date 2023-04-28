1f44352e3869d8d1d26abbaea7c38f9d3656e790
Jackson <jacksondahl27@gmail.com>
2023-04-28 18:34:34 -0400

    created basic forms for webpage, with routing

---
+++ Client/Components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div className=" bg-blue-200 shadow w-screen h-screen">
+      <h1>login</h1>
+      <a className="border shadow bg-white"href={'/homepage'}>no, click me</a>
+    </div>
+  )
+}
---
+++ Client/Components/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Signup() {
+  return (
+    <div className="container border bg-blue-200 shadow">
+      <h1>Sign up</h1>
+      <button className="border">Click me!</button>
+    </div>
+  )
+}
