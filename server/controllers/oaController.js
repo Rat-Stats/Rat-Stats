2981a7c2956d1ffbae4b6ecadd16df40ca42f10f
Camina Drummer <drummer.camina@pm.me>
2023-04-29 18:51:11 -0400

    Complete OAuth controllers and fetching GitHub user data

--- server/controllers/oaController.js
+++ server/controllers/oaController.js
@@ -1,8 +1,64 @@
+require('dotenv').config();
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const STATE = 'test_state';
+
 module.exports = {
 
-  oaLogin: (req, res, next) => {
-    console.log('Running oaLogin middleware');
+  oaCheckState: (req, res, next) => {
+    console.log('Entering onCheckState middleware');
+    console.log(req.query);
+    // Grab url query parameters from the request object
+    const { state } = req.query;
+    // Check if state from response matches what we sent to catch third party attacks
+    if (state === STATE) console.log('state === STATE');
+    else return next({ log: 'State does not match.', status: 500, message: 'OAuth error.'})
+    // If state matches continue
+    return next();
+  },
+
+  oaRequestAccessToken: (req, res, next) => {
+    console.log('Running oaRequestAccessToken middleware');
+    const { code } = req.query;
+    const githubUrl = `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}`;
+    fetch(githubUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+    .then((data) => data.json())
+    .then((parsed) => {
+      console.log('Access token: ', parsed.access_token);
+      res.locals.accessToken = parsed.access_token;
+      return next();
+    })
+    .catch((err) => next({ log: err, message: 'OAuth error.'}))
+  },
+
+  oaGetUserData: (req, res, next) => {
+    console.log('Running oaGetUserData middleware');
+    const authString = `Bearer ${res.locals.accessToken}`; 
+    console.log(authString);
+
+    fetch('https://api.github.com/user', {
+      method: "GET",
+      headers: {
+        "Authorization": authString
+      }
+    })
+    .then((data) => data.json())
+    .then((parsed) => {
+      console.log('user data: ', parsed);
+      res.locals.userData = parsed;
+      next();
+    })
+    .catch((err) => next({ log: err, message: 'OAuth error.' }))
+  },
+
+  oaSaveUserData: (req, res) => {
+    console.log('Running oaSaveUserData middleware.');
+    const { login, id, avatar_url } = res.locals.userData;
     return next();
   }
-
 }
--- server/routes/oaRouter.js
+++ server/routes/oaRouter.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const oaController = require('../controllers/oaController.js')
+require('dotenv').config();
 
-// OAuth login route
-router.use('/login', oaController.oaLogin, (req, res) => {
-  console.log('Hit oaRouter.js /login router callback.');
-  res.status(200).json('Received response from /oauth/login');
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SILENT = process.env.CLIENT_SILENT;
+const STATE = 'test_state'
+
+// Direct user's OAuth login request to GitHub 
+/*
+  Step 1: Redirect user login request to GitHub login page, providing our app's
+  client_id (which we got by registering our app on GitHub) as a url parameter
+*/
+router.use('/login', (req, res) => {
+  res.status(200).redirect(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&state=${STATE}`);
+});
+
+router.use('/redirected', oaController.oaCheckState, oaController.oaRequestAccessToken, oaController.oaGetUserData, (req, res) => {
+    console.log('Hit oaRouter.js /redirected router callback!');
+    res.status(200).json('Received response from /oauth/redirected');
+  });
+
+router.use('/loggedin', (req, res) => {
+  console.log('Hit oaRouter.js /loggedin router callback');
+  res.status(200).json('Logged in and redirecting..');
 });
 
 module.exports = router;
