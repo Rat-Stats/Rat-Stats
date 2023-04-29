fd754b64ebf318b07fe61f5dc6bfb0bafb34174d
Jackson <jacksondahl27@gmail.com>
2023-04-29 11:01:33 -0400

    Finished basic layout for login and sign up page, initialized redux store

--- Client/Components/Homepage.jsx
+++ Client/Components/Homepage.jsx
@@ -4,14 +4,10 @@ import Counter from './counter/counter.js';
 export default function Homepage() {
   const [counter, setCounter] = useState(0);
 
-  const increment = () => {
-    setCounter(prevCounter => prevCounter + 1);
-  }
-
   return (
     <div className="container border bg-blue-200 shadow">
       <h1> I'm homepage</h1>
-      <Counter />
+      
     </div>
   )
 }
--- Client/Components/Signup.jsx
+++ Client/Components/Signup.jsx
@@ -2,9 +2,41 @@ import React from 'react';
 
 export default function Signup() {
   return (
-    <div className="container border bg-blue-200 shadow">
-      <h1>Sign up</h1>
-      <button className="border">Click me!</button>
-    </div>
+    <div className="grid bg-blue-200 shadow w-screen h-screen">
+          <div className="self-center justify-self-center border shadow bg-blue-500 p-8 rounded-xl w-1/2 h-1/2">
+            <div className="grid gap-2 justify-center content-center h-full w-full">
+              <h1 className="text-5xl font-bold text-gray-700 justify-self-center">Rat Stats</h1>
+              <h1 className="text-xl font-bold text-gray-700 justify-self-center">Sign up for an Account</h1>
+              {/*username input*/}
+              <div>
+                <input type="text" 
+                id="Username" 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Username" 
+                required/>
+              </div>
+
+              {/*password input*/}
+              <div>
+                <input type="password" 
+                id="password" 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Password" 
+                required/>
+              </div>
+              {/*re-enter password*/}
+              <div>
+                <input type="password" 
+                id="password" 
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Re-Enter Password" 
+                required/>
+              </div>
+              <div className="flex flex-row justify-center p-4">
+                <a className="border shadow bg-green-500 justify-self-center" href={'/homepage'}>Create Account</a>
+              </div>
+            </div>
+          </div>
+        </div>
   );
 };
