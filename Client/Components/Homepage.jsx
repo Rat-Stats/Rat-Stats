ff6c23d7d8057f62515045b99ce30abb248faf25
Jackson <jacksondahl27@gmail.com>
2023-04-28 20:02:02 -0400

    First working implementation of redux toolkit complete. Increments a counter

--- Client/Components/Homepage.jsx
+++ Client/Components/Homepage.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Counter from './counter/counter.js';
 
 export default function Homepage() {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(prevCounter => prevCounter + 1);
+  }
+
   return (
     <div className="container border bg-blue-200 shadow">
       <h1> I'm homepage</h1>
-      <button className="border">Click me!</button>
+      <Counter />
     </div>
   )
 }
