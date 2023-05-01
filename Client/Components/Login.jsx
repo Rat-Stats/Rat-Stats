28e80cad97f14fbbd37669631e715ff3aeacf7e1
Jackson <jacksondahl27@gmail.com>
2023-05-01 10:09:02 -0400

    added oauth button on login

--- Client/Components/Login.jsx
+++ Client/Components/Login.jsx
@@ -33,6 +33,7 @@ export default function Login() {
           <div className="flex flex-row justify-between p-4">
             <a className="border shadow bg-red-500 justify-self-center" href={'/signup'}>Signup</a>
             <a className="border shadow bg-green-500 justify-self-center" href={'/homepage'}>Login</a>
+            <a className="border shadow bg-green-500 justify-self-center" href={'/oauth/login'}>Login with Tinder</a>
           </div>
         </div>
       </div>
