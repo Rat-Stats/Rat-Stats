46117a4c4337867c60c1bc2051aeb37611d48d99
Hank McGill <henrymcgill@gmail.com>
2023-05-03 14:55:27 -0400

    forcing update to indexjs

--- Client/index.js
+++ Client/index.js
@@ -1,41 +1,33 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './Components/App';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './styles.css';
 import ReactDOM from 'react-dom/client';
 import Login from './Components/Login';
 import Homepage from './Components/Homepage';
 import Signup from './Components/Signup';
-import Profile from './Components/Profile';
-import ErrorPage from './Components/Error';
 
 import store from './store.js';
 import { Provider } from 'react-redux';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: '/homepage',
-    element: <Homepage />
-  },
-  {
-    path: '/signup',
-    element: <Signup />
-  },
-  {
-    path: '/profile',
-    element: <Profile />
-  },
-])
+	{
+		path: '/',
+		element: <Login />,
+	},
+	{
+		path: '/homepage',
+		element: <Homepage />,
+	},
+	{
+		path: '/signup',
+		element: <Signup />,
+	},
+]);
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <RouterProvider router={router}/>
-  </Provider>
+	<Provider store={store}>
+		<RouterProvider router={router} />
+	</Provider>
 );
