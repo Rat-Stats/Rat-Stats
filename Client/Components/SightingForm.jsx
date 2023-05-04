ee36a4b4a568373139f1bbaf2463baf8627f0d90
Hank McGill <henrymcgill@gmail.com>
2023-05-04 17:07:59 -0400

    single source of truth moving forward - copy w rendered rats

--- Client/Components/SightingForm.jsx
+++ Client/Components/SightingForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import axios from 'axios';
+
 export default function SightingForm ({addToMarkerList}) {
   const dispatch = useDispatch();
   // this are selectors that we use with redux in order to grab the lattitude
@@ -19,8 +21,27 @@ export default function SightingForm ({addToMarkerList}) {
 
   }
 
+  const sendEmail = async (data) => {
+    try {
+      const response = await axios.post('/api/send-email', data);
+      console.log('Email sent successfully!');
+      console.log(response);
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
+  };
+  
   return (
       <div className="flex flex-row justify-around w-full ">You found a RAT
+      <button onClick={()=>{
+        
+        console.log("TEST SEND")
+        
+        sendEmail({
+    to: 'henrymcgill@gmail.com',
+    subject: 'Test email',
+    message: 'This is a test email from my React app!'
+  })}}>click to send</button>
       </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -53,6 +53,73 @@ app.use('/user', userRouter);
 // 	res.status(200).sendFile(path.join(__dirname, '../build/index.html'));
 // });
 
+// begin email experiment
+const nodemailer = require('nodemailer');
+var myemail = process.env.SENDER_EMAIL;
+var mypassword = process.env.APPLICATION_PASSWORD;
+
+function sendEmail({ recipient_email, OTP }) {
+	return new Promise((resolve, reject) => {
+		const transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: myemail,
+				pass: mypassword,
+			},
+		});
+
+		const mail_configs = {
+			from: myemail,
+			to: recipient_email,
+			subject: 'KODING 101 PASSWORD RECOVERY',
+			html: `<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - OTP Email Template</title>
+  
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+  <div style="margin:50px auto;width:70%;padding:20px 0">
+    <div style="border-bottom:1px solid #eee">
+      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Koding 101</a>
+    </div>
+    <p style="font-size:1.1em">Hi,</p>
+    <p>Thank you for choosing Koding 101. Use the following OTP to complete your Password Recovery Procedure. OTP is valid for 5 minutes</p>
+    <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${OTP}</h2>
+    <p style="font-size:0.9em;">Regards,<br />Koding 101</p>
+    <hr style="border:none;border-top:1px solid #eee" />
+    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+      <p>Koding 101 Inc</p>
+      <p>1600 Amphitheatre Parkway</p>
+      <p>California</p>
+    </div>
+  </div>
+</div>
+<!-- partial -->
+  
+</body>
+</html>`,
+		};
+		transporter.sendMail(mail_configs, function (error, info) {
+			if (error) {
+				console.log(error);
+				return reject({ message: `An error has occured` });
+			}
+			return resolve({ message: 'Email sent succesfuly' });
+		});
+	});
+}
+
+app.get('/email', (req, res) => {
+	sendEmail()
+		.then((response) => res.send(response.message))
+		.catch((error) => res.status(500).send(error.message));
+});
+// end email experiment
+
 // Global error handling
 ////// Create default error object
 const defaultErr = {
